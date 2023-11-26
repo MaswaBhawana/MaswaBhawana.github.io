95f688a099903f5735c2ff5bac19b1561b1fb2b1
MaswaBhawana <144839580+MaswaBhawana@users.noreply.github.com>
2023-11-25 23:26:56 -0500

    fourth commit
    Added the alternative text for each image file.

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -8,7 +8,10 @@ const overlay = document.querySelector('.overlay');
 const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 
-
+/* Declaring the alternative text for each image file */
+const alt = {
+    'pic1.jpg' : 'Eye', 'pic2.jpg' : 'Wave', 'pic3.jpg' : 'Flower', 'pic4.jpg' : 'Painting', 'pic5.jpg' : 'Butterfly'
+}
 
 
 /* Looping through images */
