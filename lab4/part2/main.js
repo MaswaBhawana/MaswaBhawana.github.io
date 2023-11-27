1baa00dc0bcf8c0d8d6de8db69fa69453e47020d
MaswaBhawana <144839580+MaswaBhawana@users.noreply.github.com>
2023-11-26 21:40:25 -0500

    final commit
    This is the completed code

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -5,20 +5,26 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+
 
 
 /* Declaring the alternative text for each image file */
 const alt = {
-    'pic1.jpg' : 'Eye', 'pic2.jpg' : 'Wave', 'pic3.jpg' : 'Flower', 'pic4.jpg' : 'Painting', 'pic5.jpg' : 'Butterfly'
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Rock that looks like a wave',
+  'pic3.jpg' : 'Purple and white pansies',
+  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+  'pic5.jpg' : 'Large moth on a leaf'
 }
+
 
 
 /* Looping through images */
 for (const image of images) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', `images/${image}`);
-    newImage.setAttribute('alt', alts[image]);
+    newImage.setAttribute('alt', alt[image]);
     thumbBar.appendChild(newImage);
     newImage.addEventListener('click', e => {
       displayedImage.src = e.target.src;
